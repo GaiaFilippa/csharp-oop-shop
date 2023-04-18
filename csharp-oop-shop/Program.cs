b1ccf6764d7e9018494b0eac6559dfac1df98599
GaiaFilippa <gaiafilippa2001@gmail.com>
2023-04-18 13:59:14 +0200

    Aggiungere i file di progetto.

---
+++ csharp-oop-shop/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Nel progetto csharp-oop-shop, creare la classe Prodotto che gestisce i prodotti dello shop.
+Un prodotto è caratterizzato da:
+- codice (numero intero)
+- nome
+- descrizione
+- prezzo
+- iva
+
+Usate opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter e setter
+(o eventualmente le properties) ed eventuali altri metodi di “utilità” per fare in modo che:
+
+- Alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
+- Il codice prodotto sia accessibile solo in lettura
+- Gli altri attributi siano accessibili sia in lettura che in scrittura
+- Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
+- Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
+
+Testate poi i vostri oggetti Prodotto, istanziandoli e provando ad interagirci per testare tutti i metodi che avete previsto.
+ */
+
+
